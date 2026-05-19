50ae3b359c417f6e061e872ed865818fd5a0af03
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 16:22:29 -0400

    ComunaDTO

--- MicroCliente/src/main/java/com/example/MicroCliente/dto/ComunaDTO.java
+++ MicroCliente/src/main/java/com/example/MicroCliente/dto/ComunaDTO.java
@@ -1,10 +1,3 @@
-package com.example.MicroCliente.dto;
-
-import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 public class ComunaDTO {
 
     @Data
@@ -12,19 +5,20 @@ public class ComunaDTO {
     @AllArgsConstructor
     public static class Request {
 
-    @NotNull(message = "El ID de la comuna es obligatorio")
-    @Min(value = 1, message = "El ID de la comuna debe ser mayor a 0")
-    private Integer id_comuna;
+        @NotNull(message = "El ID de la comuna es obligatorio")
+        @Min(value = 1, message = "El ID de la comuna debe ser mayor a 0")
+        private Integer id_comuna;
 
-    @NotBlank(message = "El nombre de la comuna es obligatorio")
-    @Size(min = 3, max = 100, message = "El nombre de la comuna debe tener entre 3 y 100 caracteres")
-    private String nombre;
+        @NotBlank(message = "El nombre de la comuna es obligatorio")
+        @Size(min = 3, max = 100, message = "El nombre de la comuna debe tener entre 3 y 100 caracteres")
+        private String nombre;
     }
 
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Response {
+
         private Integer id_comuna;
         private String nombre;
     }
