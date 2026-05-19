23ce38a17f5b999623c041f24bc2a7f720fe515f
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 13:21:56 -0400

    Add files via upload

---
+++ MicroCliente/src/test/java/com/example/MicroCliente/MicroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
