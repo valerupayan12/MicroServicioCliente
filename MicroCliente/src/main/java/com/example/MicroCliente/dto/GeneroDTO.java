cebf991d2c1bcf9bf3c95006172795c94cd527c0
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 16:23:09 -0400

    GeneroDTO

--- MicroCliente/src/main/java/com/example/MicroCliente/dto/GeneroDTO.java
+++ MicroCliente/src/main/java/com/example/MicroCliente/dto/GeneroDTO.java
@@ -12,20 +12,21 @@ public class GeneroDTO {
     @AllArgsConstructor
     public static class Request {
 
-    @NotNull(message = "El ID del género es obligatorio")
-    @Min(value = 1, message = "El ID del género debe ser mayor a 0")
-    private Integer id_genero;
+        @NotNull(message = "El ID del género es obligatorio")
+        @Min(value = 1, message = "El ID del género debe ser mayor a 0")
+        private Integer id_genero;
 
-    @NotBlank(message = "El nombre del género es obligatorio")
-    @Size(min = 3, max = 50, message = "El nombre del género debe tener entre 3 y 50 caracteres")
-    private String nombre_genero;
+        @NotBlank(message = "El nombre del género es obligatorio")
+        @Size(min = 3, max = 50, message = "El nombre del género debe tener entre 3 y 50 caracteres")
+        private String nombre_genero;
     }
 
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Response {
-        private Integer id;
-        private String descripcion;
+
+        private Integer id_genero;
+        private String nombre_genero;
     }
 }
