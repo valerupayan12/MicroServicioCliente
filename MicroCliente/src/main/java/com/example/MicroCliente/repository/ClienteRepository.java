2911e26094db2c47d9bccf6ec5cbaada204d0c24
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 16:13:18 -0400

    Afinando detalles

--- MicroCliente/src/main/java/com/example/MicroCliente/repository/ClienteRepository.java
+++ MicroCliente/src/main/java/com/example/MicroCliente/repository/ClienteRepository.java
@@ -7,15 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.MicroCliente.model.Cliente;
-import com.example.MicroCliente.model.Genero;
-
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-     
-    Optional<Cliente> findbyId(int id_cliente);
 
-    boolean existexistsById(int id_cliente);
+    Optional<Cliente> findById(Integer id_cliente);
 
-    List<Cliente> findByGeneroId(Genero generoid);
+    boolean existsById(Integer id_cliente);
+
+    List<Cliente> findByIdGenero(Integer id_genero);
 }
